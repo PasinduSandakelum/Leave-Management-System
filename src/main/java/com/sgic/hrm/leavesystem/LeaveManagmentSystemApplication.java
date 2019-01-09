195505d06bcbb43769ae659b4033bdfe830cbb4f
Thiruparan <thirupparan1994@gmail.com>
2019-01-09 16:33:43 +0530

    aws war file created

--- src/main/java/com/sgic/hrm/leavesystem/LeaveManagmentSystemApplication.java
+++ src/main/java/com/sgic/hrm/leavesystem/LeaveManagmentSystemApplication.java
@@ -2,9 +2,15 @@ package com.sgic.hrm.leavesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LeaveManagmentSystemApplication {
+public class LeaveManagmentSystemApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LeaveManagmentSystemApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeaveManagmentSystemApplication.class, args);
